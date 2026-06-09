218ba4fa7ab4ecf191d47433498b1a727af9880c
Aylin <thisisaylinasadi@gmail.com>
2026-06-09 21:45:36 +0400

    Initial Commit

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Keyboard.current != null && Keyboard.current.rightArrowKey.isPressed)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Keyboard.current != null && Keyboard.current.leftArrowKey.isPressed)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+    }
+}
